d5cc2a76705f73ad8f6ea70220241c2e052a9b5f
jorpac <36743869+jorpac@users.noreply.github.com>
2020-02-27 12:12:18 +0000

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,8 +1,5 @@
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.*;
 
 public class Client {
     public static void main(String args[]) throws IOException {
@@ -10,8 +7,19 @@ public class Client {
             System.out.println("Wrong number of args");
             return;
         }
-        DatagramSocket dtSocket = new DatagramSocket(Integer.parseInt(args[1]));
+        MulticastSocket dtSocket = new MulticastSocket();
         String oper = args[2];
+        dtSocket.setTimeToLive(1);
+        dtSocket.joinGroup(InetAddress.getByName(args[0]));
+        dtSocket.connect(InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
+        byte[] buf2 = new byte[1024];
+        DatagramPacket dtIPPacketRec = new DatagramPacket(buf2, 1024);
+
+        System.out.println(args[0]);
+
+        dtSocket.receive(dtIPPacketRec);
+        System.out.println(dtIPPacketRec.getData().toString());
+
         DatagramPacket dtPacket = new DatagramPacket((args[2] + " " + args[3]).getBytes(), args[2].length() + args[3].length(), InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
         byte[] buf = new byte[1024];
         DatagramPacket dtPacketRec = new DatagramPacket(buf, 1024);
